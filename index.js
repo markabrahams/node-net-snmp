a0eadc5a784209f34d65500444b291608e9f8c9c
Ray Solomon <raybsolomon@gmail.com>
2013-11-23 18:46:28 -0800

    add stack trace info to exception classes

--- index.js
+++ index.js
@@ -101,12 +101,14 @@ var Version2c = 1;
 function ResponseInvalidError (message) {
 	this.name = "ResponseInvalidError";
 	this.message = message;
+	Error.captureStackTrace(this, ResponseInvalidError);
 }
 util.inherits (ResponseInvalidError, Error);
 
 function RequestInvalidError (message) {
 	this.name = "RequestInvalidError";
 	this.message = message;
+	Error.captureStackTrace(this, RequestInvalidError);
 }
 util.inherits (RequestInvalidError, Error);
 
@@ -114,12 +116,14 @@ function RequestFailedError (message, status) {
 	this.name = "RequestFailedError";
 	this.message = message;
 	this.status = status;
+	Error.captureStackTrace(this, RequestFailedError);
 }
 util.inherits (RequestFailedError, Error);
 
 function RequestTimedOutError (message) {
 	this.name = "RequestTimedOutError";
 	this.message = message;
+	Error.captureStackTrace(this, RequestTimedOutError);
 }
 util.inherits (RequestTimedOutError, Error);
 
