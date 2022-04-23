b01297016059b86e7728576e3867a19d4e48b3ee
Mark Abrahams <mark@abrahams.co.nz>
2022-04-23 23:34:10 +1200

    Fix logic for v3 time syncronization needed

--- index.js
+++ index.js
@@ -2211,7 +2211,7 @@ Session.prototype.onMsg = function (buffer) {
 				return;
 			}
 			req.originalPdu.contextName = this.context;
-			var timeSyncNeeded = ! message.msgSecurityParameters.msgAuthoritativeEngineBoots || ! message.msgSecurityParameters.msgAuthoritativeEngineTime;
+			var timeSyncNeeded = ! message.msgSecurityParameters.msgAuthoritativeEngineBoots && ! message.msgSecurityParameters.msgAuthoritativeEngineTime;
 			this.sendV3Req (req.originalPdu, req.feedCb, req.responseCb, req.options, req.port, timeSyncNeeded);
 		}
 	} else if ( this.proxy ) {
